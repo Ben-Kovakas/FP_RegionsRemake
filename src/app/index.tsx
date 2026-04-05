4b9a7d5328427c1c24e1b5de114bea52c8f17bbb
Ben-Kovakas <benjaminkovakas@gmail.com>
2026-04-05 18:51:00 -0500

    widget grid structure layout

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,16 @@
-import { Text, View, StyleSheet } from "react-native";
+import GridContainer from "@/components/widgetGrid/GridContainer";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
-    </View>
+    
+    <GridContainer headerContent={<Text style={{ fontSize: 24, fontWeight: 'bold' }}>My Dashboard</Text>}>
+      {/* Example widgets */}
+      <View style={{ backgroundColor: 'tomato', borderRadius: 8, flex: 1, margin: 4, height: 100 }} />
+      <View style={{ backgroundColor: 'skyblue', borderRadius: 8, flex: 1, margin: 4, height: 100 }} />
+      </GridContainer>
+
+
   );
 }
 
